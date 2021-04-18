2f349717010e7d577e273ea705ac69b303c9e981
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-04-18 20:02:42 +0900

    オブジェクト練習

---
+++ 3_semi/week1/object.js
@@ -0,0 +1,11 @@
+const ryosuke = {
+  name: "望月僚介",
+  age: 21,
+  university: "駒澤大学",
+  grade: 4,
+  faculty: "グローバルメディアスタディーズ",
+  hobby: ["一人旅", "カメラ", "読書"],
+  sayHello: function() {
+    console.log(`こんにちは、${ryosuke.name}です`)
+  },
+}
