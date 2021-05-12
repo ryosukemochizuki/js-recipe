a2638d6dbbd8b4d4322f9884209729e4130ec4f4
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-05-12 11:32:40 +0900

    Vueパレット2

--- 1_workspace/3/Vue パレット/main.js
+++ 1_workspace/3/Vue パレット/main.js
@@ -5,14 +5,33 @@ new Vue({
     greenNum: 0,
     blueNum: 200,
     opacityNum: 0.5,
+    // 選択した色を残しておく
+    colors: [],
   },
   methods: {
+    // マウスがいる場所を更新(mousemoveイベント)
     changeColor: function(e) {
       this.redNum = e.offsetY
       this.greenNum = e.offsetX
     },
+    // 固定の色を保存(クリックイベント)
+    createMini: function() {
+      const color = {
+        red: this.redNum,
+        green: this.greenNum,
+        blue: this.blueNum,
+        opacity: this.opacityNum,
+      }
+      this.colors.push(color)
+    },
+    // 本パレットの色をミニ色に(miniをクリック)
+    showMiniColor: function(color) {
+      this.redNum = color.red
+      this.greenNum = color.green
+    },
   },
   computed: {
+    // dataの色関係が変わったら自動更新(本パレット)
     showColor: function() {
       return {
         backgroundColor: `rgba(${this.redNum}, ${this.greenNum}, ${this.blueNum}, ${this.opacityNum})`,
